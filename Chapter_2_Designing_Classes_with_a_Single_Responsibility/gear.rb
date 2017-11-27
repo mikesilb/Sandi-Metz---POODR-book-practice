238f9356c4e1adf006a8c8b2288a9eb20cd02d37
Michael Silberstein <mikesilb123@gmail.com>
2017-11-27 13:50:23 -0500

    Ch 2 - Bicycle gear example - Altered wheel size

--- Chapter_2_Designing_Classes_with_a_Single_Responsibility/gear.rb
+++ Chapter_2_Designing_Classes_with_a_Single_Responsibility/gear.rb
@@ -1,16 +1,21 @@
 class Gear
-  attr_reader :chainring, :cog
-  def initialize(chainring, cog)
+  attr_reader :chainring, :cog, :rim, :tire
+  def initialize(chainring, cog, rim, tire)
     @chainring = chainring
     @cog = cog
+    @rim = rim
+    @tire = tire
   end
   def ratio
     chainring/cog.to_f
   end
+  def gear_inches
+    ratio * (rim + (tire * 2))
+  end
 end
 
-the_initial_gear = Gear.new(67, 13)
-puts the_initial_gear.ratio
+the_initial_bike_setup = Gear.new(52, 11, 26, 1.5)
+puts the_initial_bike_setup.gear_inches
 
-my_second_gear = Gear.new(85, 67)
-puts my_second_gear.ratio
+my_second_bike_setup = Gear.new(52, 11, 24, 1.25)
+puts my_second_bike_setup.gear_inches
