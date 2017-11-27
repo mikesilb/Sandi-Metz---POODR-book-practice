961037754808c0ad7c6a5f3aacb87db5305870f7
Michael Silberstein <mikesilb123@gmail.com>
2017-11-27 14:17:28 -0500

    Ch 2 - Bicycle gear example - Got rid of tire parameters. Focus on gear

--- Chapter_2_Designing_Classes_with_a_Single_Responsibility/gear.rb
+++ Chapter_2_Designing_Classes_with_a_Single_Responsibility/gear.rb
@@ -1,21 +1,10 @@
 class Gear
-  attr_reader :chainring, :cog, :rim, :tire
-  def initialize(chainring, cog, rim, tire)
+  attr_reader :chainring, :cog
+  def initialize(chainring, cog)
     @chainring = chainring
     @cog = cog
-    @rim = rim
-    @tire = tire
   end
   def ratio
     chainring/cog.to_f
   end
-  def gear_inches
-    ratio * (rim + (tire * 2))
-  end
 end
-
-the_initial_bike_setup = Gear.new(52, 11, 26, 1.5)
-puts the_initial_bike_setup.gear_inches
-
-my_second_bike_setup = Gear.new(52, 11, 24, 1.25)
-puts my_second_bike_setup.gear_inches
